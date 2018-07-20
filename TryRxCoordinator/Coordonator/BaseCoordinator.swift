9a0c94a08ff0c0a6139d3cdc7a990f26ea3189c8
David <yoxisem544@gmail.com>
2018-07-20 18:17:57 +0800

    - add deeplink option - start with option, coordinate with an option

--- TryRxCoordinator/Coordonator/BaseCoordinator.swift
+++ TryRxCoordinator/Coordonator/BaseCoordinator.swift
@@ -24,15 +24,23 @@ public class BaseCoordinator<CoordinationResult> {
   }
 
   public func coordinate<R>(to coordinator: BaseCoordinator<R>) -> Observable<R> {
+    return coordinate(to: coordinator, with: nil)
+  }
+
+  public func coordinate<R>(to coordinator: BaseCoordinator<R>, with option: DeepLinkOption?) -> Observable<R> {
     store(coordinator: coordinator)
     return
       coordinator
-        .start()
+        .start(with: option)
         .do(onNext: { [weak self] _ in self?.free(coordinator: coordinator) })
   }
 
   public func start() -> Observable<CoordinationResult> {
-    fatalError("start hasn't been implemented...")
+    return start(with: nil)
+  }
+
+  public func start(with option: DeepLinkOption?) -> Observable<CoordinationResult> {
+    fatalError("start(with:) hasn't been implemented...")
   }
 
 }
---
+++ TryRxCoordinator/Coordonator/DeepLinkOption.swift
@@ -0,0 +1,13 @@
+//
+//  DeepLinkOption.swift
+//  TryRxCoordinator
+//
+//  Created by David on 2018/7/20.
+//  Copyright © 2018年 David. All rights reserved.
+//
+
+import Foundation
+
+public enum DeepLinkOption {
+  case someCondition
+}
